7f5894a191db2b0bd88c29dc0f7dfb7b3e4d67d1
R-Crypt <68041569+R-Crypt@users.noreply.github.com>
2025-01-05 19:03:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click', () => {
+  const code = document.querySelector('textarea').value;
+  alert('Code submitted!');
+  // Add logic to validate code and award points
+});
